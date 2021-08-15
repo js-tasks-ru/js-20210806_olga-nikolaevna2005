75d0768175a5f27e29dd79220f66565e01ad541e
Olga <olga-nikolaevna2005@mail.ru>
2021-08-15 18:10:08 +0300

    omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
-};
+    let new_fruits = new Object(); 	
+    fields.forEach(function(elem) {
+        if (obj.hasOwnProperty(elem)) {delete obj[elem];}
+    });
+    return new_fruits = Object.fromEntries(Object.entries(obj)); 
+}
